c25348ce9c89a8b0e648e85a918409a644449f4e
Diego <94410736+DiegoEzequiel-02@users.noreply.github.com>
2025-11-15 21:09:09 -0300

    Delete funcion.h

--- funcion.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCION_H_INCLUDED
-#define FUNCION_H_INCLUDED
-#define AMINUS(x)((x >= 'A' && x<='Z') ? (x+32) : (x))
-#define ESLETRA(x)((x >= 'A' && x<='Z') || (x >= 'a' && x<='z'))
-#include "tPila.h"
-
-#endif // FUNCION_H_INCLUDED
